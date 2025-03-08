1492e2bdb4885aec8180b82ad9f1aaa68b3609f1
Sevriukoff <igor.sevriukoff@yandex.ru>
2025-03-08 13:23:56 +0700

    Added minimal client logic for sending and receiving requests

--- Program.cs
+++ Program.cs
@@ -1,9 +1,46 @@
-﻿namespace Sevriukoff.AsciiArena.Client;
+﻿using System.Net.Sockets;
+using System.Text;
+
+namespace Sevriukoff.AsciiArena.Client;
 
 class Program
 {
-    static void Main(string[] args)
+    private const string ServerIp = "127.0.0.1";
+    private const int Port = 8080;
+    
+    static async Task Main(string[] args)
     {
-        
+        try
+        {
+            var client = new TcpClient();
+            await client.ConnectAsync(ServerIp, Port);
+            Console.WriteLine("Подключено к серверу.");
+
+            var stream = client.GetStream();
+
+            var message = "Hello, server!";
+            var data = Encoding.UTF8.GetBytes(message);
+            await stream.WriteAsync(data);
+            Console.WriteLine($"Отправлено: {message}");
+
+            var buffer = new byte[1024];
+            
+            while (true)
+            {
+                var bytesRead = await stream.ReadAsync(buffer);
+                
+                if (bytesRead == 0)
+                    break;
+                
+                var response = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                Console.WriteLine("Received: " + response);
+            }
+
+            client.Close();
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка: " + ex.Message);
+        }
     }
 }
